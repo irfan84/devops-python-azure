03b75fd1b0ec8408f1a3cb5e849096d8fd938135
Irfan Shakeel <frozen.fyre@gmail.com>
2026-02-06 17:41:36 +1300

    Fix App Service slots and Key Vault access policy

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -2,19 +2,37 @@ locals {
   name = "${var.project}-${var.environment}"
 }
 
+# --------------------
+# Resource Group
+# --------------------
 resource "azurerm_resource_group" "app" {
   name     = "rg-${local.name}"
   location = var.location
 }
 
+# --------------------
+# App Service Plan (S1 supports slots)
+# --------------------
 resource "azurerm_service_plan" "plan" {
   name                = "asp-${local.name}"
   location            = azurerm_resource_group.app.location
   resource_group_name = azurerm_resource_group.app.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "S1"
 }
 
+# --------------------
+# Random suffix for globally-unique names
+# --------------------
+resource "random_string" "suffix" {
+  length  = 5
+  upper   = false
+  special = false
+}
+
+# --------------------
+# Linux Web App
+# --------------------
 resource "azurerm_linux_web_app" "app" {
   name                = "app-${local.name}-${random_string.suffix.result}"
   location            = azurerm_resource_group.app.location
@@ -29,11 +47,14 @@ resource "azurerm_linux_web_app" "app" {
   }
 
   app_settings = {
-    WEBSITES_PORT                    = "8000"
-    SCM_DO_BUILD_DURING_DEPLOYMENT    = "true"
+    WEBSITES_PORT                 = "8000"
+    SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
   }
 }
 
+# --------------------
+# Staging Slot
+# --------------------
 resource "azurerm_linux_web_app_slot" "staging" {
   name           = "staging"
   app_service_id = azurerm_linux_web_app.app.id
@@ -50,11 +71,10 @@ resource "azurerm_linux_web_app_slot" "staging" {
   }
 }
 
-resource "random_string" "suffix" {
-  length  = 5
-  upper   = false
-  special = false
-}
+# --------------------
+# Key Vault
+# --------------------
+data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
   name                       = "kv-${local.name}-${random_string.suffix.result}"
@@ -62,14 +82,40 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name        = azurerm_resource_group.app.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
+
   purge_protection_enabled   = false
   soft_delete_retention_days = 7
+
+  # Explicitly use access policies (simpler for learning)
+  enable_rbac_authorization = false
 }
 
-data "azurerm_client_config" "current" {}
+# --------------------
+# Key Vault Access Policy (for Terraform identity)
+# --------------------
+resource "azurerm_key_vault_access_policy" "current_user" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
 
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete"
+  ]
+}
+
+# --------------------
+# Key Vault Secret
+# --------------------
 resource "azurerm_key_vault_secret" "app_secret" {
   name         = "APP-SECRET"
   value        = "change-me"
   key_vault_id = azurerm_key_vault.kv.id
+
+  # Prevent race condition
+  depends_on = [
+    azurerm_key_vault_access_policy.current_user
+  ]
 }
